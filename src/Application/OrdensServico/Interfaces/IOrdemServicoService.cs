0fa27ed6a568967e496cf740ed67258b87697790
lucas kenji gashu <kenjigashu@gmail.com>
2026-04-21 22:13:06 -0300

    Faltou a interface

---
+++ src/Application/OrdensServico/Interfaces/IOrdemServicoService.cs
@@ -0,0 +1,20 @@
+namespace Application.OrdensServico.Interfaces;
+
+using Application.OrdensServico.DTOs;
+
+public interface IOrdemServicoService
+{
+    public Task<IEnumerable<OrdemServicoResponseDto>> GetAll();
+    public Task<OrdemServicoResponseDto> ObterPorId(Guid id);
+    public Task Criar(OrdemServicoCreateDto dto);
+    public Task Deletar(OrdemServicoDeleteDto dto);
+    public Task AdicionarPecas(OrdemServicoAdicionaPecaDto dto);
+    public Task AdicionarServicos(OrdemServicoAdicionaServicoDto dto);
+    public Task EnviarOrcamento(OrdemServicoEnviarOrcamentoDto dto);
+    public Task AprovarOrcamento(OrdemServicoAprovarOrcamentoDto dto);
+    public Task IniciarDiagnostico(OrdemServicoIniciarDiagnosticoOrcamentoDto dto);
+    public Task FinalizarDiagnostico(OrdemServicoFinalizarDiagnosticoOrcamentoDto dto);
+    public Task IniciarExecucao(OrdemServicoIniciarExecucaoOrcamentoDto dto);
+    public Task FinalizarExecucao(OrdemServicoFinalizarExecucaoOrcamentoDto dto);
+    public Task EntregarVeiculo(OrdemServicoEntregarVeiculoDto dto);
+}
